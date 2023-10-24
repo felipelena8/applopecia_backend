1bbda577dd106c087f82419a490f6130c0f5309f
felipelena8 <77079122+felipelena8@users.noreply.github.com>
2023-10-23 23:51:55 -0300

    Inicio

---
+++ src/main/java/com/uade/applopecia/ApplopeciaApplication.java
@@ -0,0 +1,13 @@
+package com.uade.applopecia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplopeciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplopeciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uade/applopecia/ApplopeciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.applopecia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplopeciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
